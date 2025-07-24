fa0d88ab9e56ecd000256507d39cf4fe119162c1
d4branch <169065994+d4branch@users.noreply.github.com>
2025-07-24 08:28:13 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -43,7 +43,7 @@ window.onload = async () => {
     }).addTo(map);
 
     try {
-    const response = await fetch("https://raw.githubusercontent.com/d4branch/gateway-property-map/main/cleaned_properties_with_coords.json?cb=" + Date.now());
+    const response = await fetch("https://raw.githubusercontent.com/d4branch/gateway-property-map/main/cleaned_properties_with_coords_colored.json?cb=" + Date.now());
     const properties = await response.json();
     addMarkers(map, properties);
 } catch (error) {
